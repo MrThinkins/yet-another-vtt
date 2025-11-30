97aefe7f1797d9862471c5e6ae2db3414daede88
alowtron <alowtron@gmail.com>
2025-11-30 10:09:14 -0500

    fixed the build errors on the layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Quintessential, Charm, Geist, Geist_Mono } from "next/font/google";
+import { Quintessential } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -8,19 +8,20 @@ const  quintessential = Quintessential({
   weight: '400',
   subsets: ['latin']
 })
-const charm = Charm({
-  weight: '400',
-  subsets: ['latin']
-})
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const charm = Charm({
+//   weight: '400',
+//   subsets: ['latin']
+// })
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Yet Another VTT",
