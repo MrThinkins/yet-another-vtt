4659639ac5aeb07aa761427672f64abeb04c511d
alowtron <alowtron@gmail.com>
2025-12-05 11:09:42 -0500

    added in a spot to store info from yet another ttrpg stuff, and attached the data from it to the character sheets

--- components/creatures/creatureSheets/addPassiveSkill.tsx
+++ components/creatures/creatureSheets/addPassiveSkill.tsx
@@ -1,27 +1,12 @@
+import { yetAnotherTTRPGPassiveSkills } from "@/components/yetAnotherTTRPGInfo/arrays"
+
 interface addPassiveSkillProps {
   show: boolean,
   onShowUpdate: (newShow: boolean) => void,
   onUpdate: (skill: object) => void
 }
 
-const passiveSkills = [
-  {
-    name: 'Extra Health',
-    effect: '+1 hit point per level.'
-  },
-  {
-    name: 'Fast Movement',
-    effect: 'Move speed is increased by 5 feet.'
-  },
-  {
-    name: 'Tough Skin',
-    effect: '+1 physical and magical damage resist.'
-  },
-  {
-    name: 'Friendly',
-    effect: 'When making a Charisma check with a creature that is friendly with you, you may  reroll any one’s that you rolled.'
-  }
-]
+const passiveSkills = yetAnotherTTRPGPassiveSkills
 
 export default function AddPassiveSkill({
   show,
