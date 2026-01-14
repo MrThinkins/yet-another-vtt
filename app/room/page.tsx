873a669de83a0ef6f57a5d3ee1626a6c9a4d58c6
alowtron <alowtron@gmail.com>
2026-01-14 08:38:32 -0500

    added room creation form and room name picking is also working properly now

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -5,6 +5,7 @@ import { useMutation, useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import Link from "next/link"
 import { usePathname } from 'next/navigation'
+import { FormEvent, useState } from "react"
 
 export default function Room() {
   const urlPath = usePathname()
@@ -12,14 +13,23 @@ export default function Room() {
   const createRoom = useMutation(api.rooms.createRoom)
   const rooms = useQuery(api.rooms.getUserRoomList, { userId: userId || ''})
 
-  function createRoomFunction() {
+  const [roomName, setRoomName] = useState<string> ("")
+
+  const createRoomFunction = (e: FormEvent) => {
+    e.preventDefault()
     if (userId) {
+      let roomNameToUse = roomName
+      if (roomName == "" || roomName == null) {
+        roomNameToUse = "Temp Room Name"
+      }
       createRoom({
         userId: userId,
-        name: "temp"
+        name: roomNameToUse
       })
+      setRoomName("")
     }
   }
+
 
   return (
     <div>
@@ -31,9 +41,23 @@ export default function Room() {
         </div>
       ))}
       <br></br>
-      <button onClick={createRoomFunction}>
-        Create New Room
-      </button>
+      <form onSubmit={createRoomFunction}>
+        <label 
+          htmlFor="roomName"
+        >
+          Room Name:
+        </label>
+        <input
+          id="roomName"
+          type="text"
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+        ></input>
+        <button type="submit">
+          Create New Room
+        </button>
+      </form>
+
     </div>
   )
 }
